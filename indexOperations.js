525e02fa5e8a1ffede6e1ab52abcb17a9d82e74b
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2020-10-31 15:16:24 +0500

    removed vulnerability

--- indexOperations.js
+++ indexOperations.js
@@ -1,7 +1,7 @@
 const MongoClient=require('mongodb').MongoClient
 const assert = require('assert');
 const dboper=require('./operataions');
-const uri = "mongodb+srv://admin:admin@node.2trvo.mongodb.net/user?retryWrites=true&w=majority";
+const uri = "";
 MongoClient.connect(uri, (err,cluster)=>{
     
     const db=cluster.db('node')
@@ -9,13 +9,13 @@ MongoClient.connect(uri, (err,cluster)=>{
     const collection='users';
     dboper.insertDocument(db,{firstName:"Mubashir",lastName:"Ali Baig"}, collection,(result)=>{
         console.log('Inserting Document:\n', result.ops);
-        dboper.findDocument(db,collection,(docs)=>{
-            console.log('Found Documents:\n',docs);
-            dboper.updateDocument(db, {firstName:'Mubashir'},{firstName:'Mudassir',lastName:'Ali Baig'},collection,(result)=>{
-                console.log('Updated Document:\n', result.result);
-                dboper.findDocument(db,collection,(docs)=>{
-                    console.log('Found Updated Documents:\n',docs);
-                    cluster.close()
+            dboper.findDocument(db,collection,(docs)=>{
+                console.log('Found Documents:\n',docs);
+                dboper.updateDocument(db, {firstName:'Mubashir'},{firstName:'Mudassir',lastName:'Ali Baig'},collection,(result)=>{
+                    console.log('Updated Document:\n', result.result);
+                        dboper.findDocument(db,collection,(docs)=>{
+                            console.log('Found Updated Documents:\n',docs);
+                            cluster.close()
                 });
             });          
         });
